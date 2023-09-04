61780d7fcad5f00d6ac18079a7c6ff9199a48b5e
FraBif <francescobif@hotmail.it>
2023-09-04 22:13:09 +0200

    Populated pivot table

--- database/migrations/2023_09_04_194812_create_buyers_table.php
+++ database/migrations/2023_09_04_194812_create_buyers_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('buyers', function (Blueprint $table) {
             $table->id();
+            $table->string("username");
+            $table->string("email");
             $table->timestamps();
         });
     }
--- database/migrations/2023_09_04_195824_create_buyers_products_table.php
+++ database/migrations/2023_09_04_195824_create_buyers_products_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('buyers_products', function (Blueprint $table) {
             $table->id();
-            $table->string("username");
-            $table->string("email");
+            $table->string('product_name');
+            $table->foreign('product_name')->references('name')->on('products');
+            $table->string('buyer_username');
+            $table->foreign('buyer_username')->references('username')->on('buyers');
             $table->timestamps();
         });
     }
