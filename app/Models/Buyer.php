b729b0ac0f75baa37e343388078d726ebc60eb40
FraBif <francescobif@hotmail.it>
2023-09-04 22:15:40 +0200

    Added many to many relations to models

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Buyer extends Model
 {
     use HasFactory;
+    public function products()
+    {
+        return $this->belongsToMany(Products::class);
+    }
 }
